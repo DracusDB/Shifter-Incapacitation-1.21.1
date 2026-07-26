5b7cb0599cc3272119ec5bc953ed82b6ddc21565
Michael <giordanomichael2@gmail.com>
2026-07-26 16:25:17 -0400

    Initial commit

---
+++ src/main/java/net/dracus/shifterincapacitation/ShifterIncapacitationClient.java
@@ -0,0 +1,10 @@
+package net.dracus.shifterincapacitation;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ShifterIncapacitationClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
